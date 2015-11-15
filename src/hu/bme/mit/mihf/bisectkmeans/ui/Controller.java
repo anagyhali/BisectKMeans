837b9af6ed505ad892cd34e707efd1f1529dbe64
anagyhali <anagyhali@gmail.com>
2015-11-15 11:49:06 +0100

    Initial commit

---
+++ src/hu/bme/mit/mihf/bisectkmeans/ui/Controller.java
@@ -0,0 +1,12 @@
+package hu.bme.mit.mihf.bisectkmeans.ui;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
+public class Controller {
+    public Label labelLoadFile;
+
+    public void onBtnLoadFileClick(ActionEvent actionEvent) {
+        labelLoadFile.setText("MEG LETT NYOMVAAA");
+    }
+}
